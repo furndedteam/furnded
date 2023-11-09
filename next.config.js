391f93481e45c02710f8a9fe305d36b1953c38b7
storm <khameleonstorm.com>
2023-11-09 08:30:15 +0100

    3

--- next.config.js
+++ next.config.js
@@ -3,23 +3,6 @@ const nextConfig = {
   images: {
     domains: ['robohash.org', 'firebasestorage.googleapis.com'],
   },
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.(webm)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next',
-            name: 'static/media/[name].[hash].[ext]',
-          },
-        },
-      ],
-    });
-
-    // Important: return the modified config
-    return config;
-  },
 };
 
 module.exports = nextConfig;
